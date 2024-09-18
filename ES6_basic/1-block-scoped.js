b8a3414bd1aa3b71d01d073d745237c2559b6d02
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-18 13:58:26 +0400

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,11 +3,13 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;  
 
   if (trueOrFalse) {
-    const task = true;     
-    const task2 = false;    
+    const newTask = true;   
+    const newTask2 = false; 
+    return [newTask, newTask2]; 
   }
 
-  return [task, task2];
+  return [task, task2]; 
 }
+
 
 
