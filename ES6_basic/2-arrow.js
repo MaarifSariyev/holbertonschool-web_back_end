b130e3aa6dbe9110632b7ab3b1ac65a6ac784f74
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-19 12:15:11 +0400

    Update 2-arrow.js

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,11 +1,11 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods = [
-      ...this.sanFranciscoNeighborhoods,
-      newNeighborhood,
-    ];
+    this.sanFranciscoNeighborhoods = [...this.sanFranciscoNeighborhoods, newNeighborhood];
+    return this.sanFranciscoNeighborhoods;
   };
 }
+
 
