0c2630ad1c681bb95066cc784bc504afcf009718
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-19 12:29:40 +0400

    Update 9-getFullBudget.js

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,3 +1,5 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
 
@@ -13,6 +15,14 @@ export default function getFullBudgetObject(income, gdp, capita) {
 
   return fullBudget;
 }
+// 9-main.js
+import getFullBudgetObject from './9-getFullBudget.js';
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars()); // Outputs: $20
+console.log(fullBudget.getIncomeInEuros());   // Outputs: 20 euros
+
 
 
 
