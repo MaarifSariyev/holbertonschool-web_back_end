d5cf8edd4857c54ed828dc51966c975435291572
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-19 10:51:23 +0400

    Update 9-getFullBudget.js

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,23 +1,17 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   
   const fullBudget = {
     ...budget,
     getIncomeInDollars() {
-      return `$${this.income}`; // Accessing income using 'this'
+      return `$${this.income}`; 
     },
     getIncomeInEuros() {
-      return `${this.income} euros`; // Same here
+      return `${this.income} euros`; 
     },
   };
 
   return fullBudget;
 }
 
-const fullBudget = getFullBudgetObject(20, 50, 10);
-
-console.log(fullBudget.getIncomeInDollars());
-console.log(fullBudget.getIncomeInEuros());
 
