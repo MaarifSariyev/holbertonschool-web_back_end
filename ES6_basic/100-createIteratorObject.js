80c802a3918bed4c48e6d3464e6729097ea530f5
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-20 18:33:34 +0400

    Update 100-createIteratorObject.js

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
 export default function createIteratorObject(report) {
-  const employees = report.allEmployees; 
+  const employees = report.allEmployees;
   const employeeList = [];
   for (const department in employees) {
     employeeList.push(...employees[department]);
