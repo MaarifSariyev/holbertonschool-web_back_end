ed0a1079b4226b00d1946038191ca4d71eb6f11d
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-23 20:14:01 +0400

    Update 0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,1 +1,13 @@
-
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+            if (success) {
+                resolve("API response received!");
+            } else {
+                reject(new Error("API call failed."));
+            }
+        }, 1000);
+    });
+}
+export default getResponseFromAPI;
