b076933fb2653c202dbd5ab4b1b453e0ee49e018
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-23 21:22:25 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,12 +4,14 @@ import uploadPhoto from './5-photo-reject';
 function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
+
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => {
       return results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
+        value: result.status === 'fulfilled' ? result.value : result.reason.message, // Change this line
       }));
     });
 }
+
 export default handleProfileSignup;
