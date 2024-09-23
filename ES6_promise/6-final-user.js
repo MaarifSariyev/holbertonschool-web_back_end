a4e09c08040a6db5079a18cf103bf9425806dab2
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-23 21:29:38 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,16 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
+  // Call the two functions to get promises
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
+  // Use Promise.allSettled to wait for both promises to settle
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => {
+      // Map the results to the desired structure
       return results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message, // Ensure no syntax error here
+        value: result.status === 'fulfilled' ? result.value : result.reason.message,
       }));
     });
 }
-
 export default handleProfileSignup;
