9355138f98d803b3be70bffb5701e747e7a533ae
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 19:34:10 +0400

    Create 5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (new.target === Building) {
+      throw new Error("Cannot instantiate abstract class Building");
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
