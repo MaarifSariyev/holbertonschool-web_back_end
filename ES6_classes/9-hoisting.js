3791cb2e94c952b6839ef401bd1350e7b317a58a
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 20:51:02 +0400

    Update 9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+// 9-hoisting.js
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -20,7 +21,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Corrected to use the parameter
+    this._holbertonClass = holbertonClass; // Fixed this line
   }
 
   get fullName() {
@@ -28,20 +29,20 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Corrected to return the instance variable
+    return this._holbertonClass; // Fixed this getter
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Fixed this line
   }
 }
 
-// Creating instances of students
+// Create students
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Exporting the list of students
+// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
