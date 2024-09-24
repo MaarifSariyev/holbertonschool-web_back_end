75f8fcd377d0d80a7e0a63ce0837c374c214f0a1
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 19:13:54 +0400

    Create 3-currency.js

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+Class Currency{
+  constructor(code,name){
+    this.code = code;
+    this.name = name;
+  };
+  get code(){
+    return this._code;
+  }
+
+  set code(value){
+    if(typeOf(value)!== 'string'){
+      Throw new TypeError("code value must be string");
+    }
+    this._code = value;
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  set name(value){
+    if(typeOf(value)!== 'string'){
+      Throw new TypeError("name must be string")
+    }
+    this._name = value
+  }
+  displayFullCurrency(){
+    return `${this.name} ${this.code}`
+  };
+}
