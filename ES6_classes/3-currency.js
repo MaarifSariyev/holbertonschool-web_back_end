45e00f332a3f695e11325301068a349ba4bcb521
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 19:16:23 +0400

    Update 3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,32 @@
-export default Class Currency{
-  constructor(code,name){
+export default class Currency {
+  constructor(code, name) {
     this.code = code;
     this.name = name;
-  };
-  get code(){
+  }
+
+  get code() {
     return this._code;
   }
 
-  set code(value){
-    if(typeOf(value)!== 'string'){
-      Throw new TypeError("code value must be string");
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
 
-  get name(){
+  get name() {
     return this._name;
   }
 
-  set name(value){
-    if(typeOf(value)!== 'string'){
-      Throw new TypeError("name must be string")
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    this._name = value
+    this._name = value;
   }
-  displayFullCurrency(){
-    return `${this.name} ${this.code}`
-  };
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
