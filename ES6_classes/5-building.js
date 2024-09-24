c85c849378b4cdb33316ea896421138104b2a650
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 20:15:55 +0400

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -14,6 +14,31 @@ export default class Building {
 
   // Abstract method
   evacuationWarningMessage() {
-   return throw new Error("Class extending Building must override evacuationWarningMessage");
+    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
+
+describe('Building class', () => {
+  it('should throw an error if evacuationWarningMessage is not overridden', () => {
+    class IncompleteBuilding extends Building {}
+
+    const building = new IncompleteBuilding(100);
+    
+    // Check that calling the method throws the expected error
+    expect(() => {
+      building.evacuationWarningMessage(); // This should throw
+    }).toThrow("Class extending Building must override evacuationWarningMessage");
+  });
+
+  it('should allow instantiation of subclasses that override evacuationWarningMessage', () => {
+    class TestBuilding extends Building {
+      evacuationWarningMessage() {
+        return "Evacuate the building!";
+      }
+    }
+
+    const building = new TestBuilding(200);
+    expect(building.sqft).toBe(200);
+    expect(building.evacuationWarningMessage()).toBe("Evacuate the building!");
+  });
+});
