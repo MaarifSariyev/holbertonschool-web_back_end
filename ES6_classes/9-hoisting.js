6f3ff9acaf243f9d2c1e8e1a8b91acc63d96f9e2
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-24 21:00:29 +0400

    Update 9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,18 +1,20 @@
+// 9-hoisting.js
 export class HolbertonClass {
-  constructor(size, location) {
-    this._size = size;
+  constructor(year, location) {
+    this._year = year;
     this._location = location;
   }
 
-  valueOf() {
-    return this._size;
+  get year() {
+    return this._year;
   }
 
-  toString() {
+  get location() {
     return this._location;
   }
 }
 
+// Creating instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -20,7 +22,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Fixed this line
+    this._holbertonClass = holbertonClass; // Corrected this line
   }
 
   get fullName() {
@@ -28,20 +30,20 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Fixed this getter
+    return this._holbertonClass; // Corrected getter
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Fixed this line
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Corrected this line
   }
 }
 
-// Create students
+// Creating instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
+// Exporting the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
