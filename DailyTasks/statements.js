adee3064a9e10219004bebe118cf12a0e19ad8ca
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-25 11:45:38 +0400

    Create statements.js

---
+++ DailyTasks/statements.js
@@ -0,0 +1,17 @@
+var x = 30;
+console.log(x)
+
+x = 15
+console.log(x)
+
+var x = 20;
+console.log(x)
+
+{
+  let y = 15;
+}
+console.log(y) //let blockdan declare olundugu blokdan kenarda not accesible
+
+const z = 16;
+z = 15
+ console.log(z) //constant deyisdirilmir
