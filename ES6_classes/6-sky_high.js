f6add77fb8f4ddcbd278a8a4f3ceb872a8f514de
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-25 13:30:36 +0400

    Update 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,37 @@
-import Building from './5-building';
+// 6-sky_high.js
+
+import Building from './5-building.js';
+
 
 export default class SkyHighBuilding extends Building {
+
   constructor(sqft, floors) {
-    super(sqft);
+
+    super(sqft); // Assign sqft to the parent class Building
+
     this._floors = floors;
+
   }
+
 
   get sqft() {
+
     return this._sqft;
+
   }
+
 
   get floors() {
+
     return this._floors;
+
   }
 
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+
+    return `Evacuate slowly the ${this._floors} floors`; // Remove the trailing period
+
   }
+
 }
