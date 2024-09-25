083fa2e42807484aee1d78c0de811b7582705d76
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-09-25 13:17:34 +0400

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,24 @@
 // 5-building.js
+
 export default class Building {
+
   constructor(sqft) {
+
     this._sqft = sqft;
-    
-    // Check if the evacuationWarningMessage method is overridden
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      return 'Class extending Building must override evacuationWarningMessage';
-    }
+
   }
+
 
   get sqft() {
+
     return this._sqft;
+
   }
 
+
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+
   }
 }
