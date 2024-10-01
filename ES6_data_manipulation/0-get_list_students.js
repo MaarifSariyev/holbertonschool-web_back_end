9df6ccd56bc0d9210e299b4ef8024e022872aeb6
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-01 20:35:22 +0400

    Update 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,9 @@
 function getListStudents() {
- return [
+    return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }, // Added trailing comma
+    ];
 }
+
 export default getListStudents;
