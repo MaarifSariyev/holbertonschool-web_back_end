0b4dcdf70b333f74bfc50215f712ede650de6475
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-02 23:59:20 +0400

    Update 1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,13 @@
-import getListStudents from "./0-get_list_students";
+import getListStudents from './0-get_list_students';
 
 const getListStudentIds = (students) => {
   if (!Array.isArray(students)) {
     return [];
   }
-  
-  return students.map(student => student.id);
+  return students.map((student) => student.id);
 };
 
 export default getListStudentIds;
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudents()));
