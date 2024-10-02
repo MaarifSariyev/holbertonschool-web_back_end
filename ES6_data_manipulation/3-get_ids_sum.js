d2738a3ffc055cfe82660e8300a947f39bd61af1
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-03 00:12:15 +0400

    Create 3-get_ids_sum.js

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+const getStudentIdsSum = (students) => {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+};
+
+export default getStudentIdsSum;
