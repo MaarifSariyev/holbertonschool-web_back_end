3348b8d783e43f7a1217b0f5b3555c14d910616b
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-04 00:17:09 +0400

    Update 10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (let [key, value] of map) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
